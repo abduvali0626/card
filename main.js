9a6953c6f7899c80af1a320b34d1e3164201e7fa
abduvali0626 <umarrrrrrrrrrrrrrr63@gmail.com>
2025-05-09 07:44:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let p = document.getElementById("p");
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+let p3 = document.getElementById("p3");
+let p4 = document.getElementById("p4");
+
+function btn1() {
+    p.textContent = "Mars is the fourth planet from the Sun. It is also known as the Red Planet, because of its orange-red appearance.[22][23] Mars is a desert-like rocky planet with a tenuous carbon dioxide (CO2) atmosphere. At the average surface level the atmospheric pressure is a few thousandths of Earth's, atmospheric temperature ranges from −153 °C (−243 °F) to 20 °C (68 °F)[24] and cosmic radiation is high. Mars retains some water, in the ground as well as thinly in the atmosphere, forming cirrus clouds, frost, larger polar regions of permafrost and ice caps (with seasonal CO2 snow), but no liquid surface water. Its surface gravity is roughly a third of Earth's or double that of the Moon. It is half as wide as Earth or twice the Moon, with a diameter of 6,779 km (4,212 mi), and has a surface area the size of all the dry land of Earth."
+}
+
+function btn2() {
+    p1.textContent = "Greece,[c] officially the Hellenic Republic,[d] is a country in Southeast Europe. Located on the southern tip of the Balkan peninsula, Greece shares land borders with Albania to the northwest, North Macedonia and Bulgaria to the north, and Turkey to the east. The Aegean Sea lies to the east of the mainland, the Ionian Sea to the west, and the Sea of Crete and the Mediterranean Sea to the south. Greece has the longest coastline on the Mediterranean Basin, featuring thousands of islands. The country comprises nine traditional geographic regions, and has a population of over 10 million. Athens is the nation's capital and its largest city, followed by Thessaloniki and Patras."
+}
+
+function btn3() {
+    p2.textContent = "Reddit (/ˈrɛdɪt/ ⓘ RED-it) is an American proprietary social news aggregation and forum social media platform. Registered users (commonly referred to as redditors) submit content to the site such as links, text posts, images, and videos, which are then voted up or down (upvoted or downvoted) by other members. Posts are organized by subject into user-created boards called subreddits. Submissions with more upvotes appear towards the top of their subreddit and, if they receive enough upvotes, ultimately on the site's front page. Reddit administrators moderate the communities. Moderation is also conducted by community-specific moderators, who are unpaid volunteers.[6] It is operated by Reddit Inc., based in San Francisco.[7][8]"
+}
+
+function btn4() {
+    p3.textContent = "Ba'athism, also spelled Baathism,[a] is an Arab nationalist ideology which advocates the establishment of a unified Arab state through the rule of a Ba'athist vanguard party operating under a revolutionary socialist framework. The ideology is officially based on the theories of the Syrian intellectuals Michel Aflaq (per the Iraqi-led Ba'ath Party), Zaki al-Arsuzi (per the Syrian-led Ba'ath Party), and Salah al-Din al-Bitar. Ba'athist leaders of the modern era include the former president of Iraq Saddam Hussein, and former presidents of Syria Hafez al-Assad and his son Bashar al-Assad."
+}
+
+function btn5() {
+    p4.textContent = "C (pronounced /ˈsiː/ – like the letter c)[6] is a general-purpose programming language. It was created in the 1970s by Dennis Ritchie and remains very widely used and influential. By design, C's features cleanly reflect the capabilities of the targeted CPUs. It has found lasting use in operating systems code (especially in kernels[7]), device drivers, and protocol stacks, but its use in application software has been decreasing.[8] C is commonly used on computer architectures that range from the largest supercomputers to the smallest microcontrollers and embedded systems."
+}
